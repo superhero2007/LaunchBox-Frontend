88d42a62a05a2ff2234c5e3cf25bfaa60a09e05e
Apollo <super.hero2017@outlook.com>
2019-05-20 17:51:05 -0400

    Deploy to the backend

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -123,7 +123,7 @@ module.exports = options => ({
         API_ENTRY_PREFIX: JSON.stringify(
           process.env.API_ENTRY_PREFIX
             ? process.env.API_ENTRY_PREFIX
-            : 'http://localhost:8080',
+            : 'http://52.87.189.185',
         ),
       },
     }),
