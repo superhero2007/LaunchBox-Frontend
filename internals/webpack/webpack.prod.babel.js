e4161521f5487d8f7faffc3bd7b350124a8568a0
Apollo <super.hero2017@outlook.com>
2019-07-03 14:20:27 -0400

    Test

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -1,7 +1,6 @@
 // Important modules this config uses
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
 const OfflinePlugin = require('offline-plugin');
 const { HashedModuleIdsPlugin } = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
@@ -118,27 +117,6 @@ module.exports = require('./webpack.base.babel')({
       test: /\.js$|\.css$|\.html$/,
       threshold: 10240,
       minRatio: 0.8,
-    }),
-
-    new WebpackPwaManifest({
-      name: 'React Boilerplate',
-      short_name: 'React BP',
-      description: 'My React Boilerplate-based project!',
-      background_color: '#fafafa',
-      theme_color: '#b1624d',
-      inject: true,
-      ios: true,
-      icons: [
-        {
-          src: path.resolve('app/brand_logo.svg'),
-          sizes: [72, 96, 128, 144, 192, 384, 512],
-        },
-        {
-          src: path.resolve('app/brand_logo.svg'),
-          sizes: [120, 152, 167, 180],
-          ios: true,
-        },
-      ],
     }),
 
     new HashedModuleIdsPlugin({
