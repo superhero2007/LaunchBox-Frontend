303c79a2a2f2f881b3891c2f2bfaf30ab7852f00
Apollo <super.hero2017@outlook.com>
2019-07-03 13:37:50 -0400

    Add favicon

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -98,7 +98,7 @@ module.exports = require('./webpack.base.babel')({
 
       // No need to cache .htaccess. See http://mxs.is/googmp,
       // this is applied before any match in `caches` section
-      excludes: ['.htaccess'],
+      excludes: ['.htaccess', 'brand_logo.svg'],
 
       caches: {
         main: [':rest:'],
